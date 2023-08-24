8f4f6e27ad43e8cb5f736b1fd2c576ebe28a13e8
greggbaker <abbasgure@googlemail.com>
2023-08-24 02:24:04 +0100

    completed 1hr mark

--- script.js
+++ script.js
@@ -31,32 +31,72 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function({starterIndex, mainIndex, time, address}){
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
     console.log(`Your order was received!
     Delivery will arrive at ${time} at ${address}.
     You ordered ${this.starterMenu[starterIndex]} with ${this.mainMenu[mainIndex]}.`);
+  },
+
+  orderPasta: function(ing1,ing2,ing3){
+
+    /*const ingredient1 = prompt('type ingredient 1:')
+    const ingredient2 = prompt('type ingredient 2:')
+    const ingredient3 = prompt('type ingredient 3:')
+    
+    console.log(`We shall make pasta, the following ingredients are required:
+    
+    ${ingredient1}, ${ingredient2} & lastly ${ingredient3}`);
+
+    we can instead also make an array of prompts taking the three ingredients then we can use the spread operator as shown below
+    */
+
+    
+    console.log(`We shall make pasta, the following ingredients are required: ${ing1}, ${ing2} & ${ing3}`)
+
   }
 };
 
+const ingredients =[
+  prompt("Ingredient 1"),
+  prompt("Ingredient 2"),
+  prompt("Ingredient 3"),
 
-
-
+]
+// restaurant.orderPasta(...ingredients);
+/* _____________________________________ */
 /*Destructuring an object and inputting its values
 created the orderDelivery Method, refer to console output what occur and play with below object below and its method above */
 
-restaurant.orderDelivery({
-  time: '23:30',
-  address:'saes rd 21',
-  mainIndex: 1,
-  starterIndex: 1
-});
+// restaurant.orderDelivery({
+//   time: '23:30',
+//   address: 'saes rd 21',
+//   mainIndex: 1,
+//   starterIndex: 1,
+// });
 
-// the : is used for renaming the values for later use in console log as shown in example below
-const {name: resturantName, categories: restaurantFoodCategories,openingHours: restaurantOpeningHrs,mainMenu:restaurantMainMenu} = restaurant;
+// the : is used for renaming the values for later use in console log as shown in example below (mutation of value names)
+// const {
+//   name: resturantName,
+//   categories: restaurantFoodCategories,
+//   openingHours: restaurantOpeningHrs,
+//   mainMenu: restaurantMainMenu,
+// } = restaurant;
+
+/* _____________________________________ */
+
+/* Now to use default empty arrays to display content of the existing array if the array of menu is empty (if no menu property exists) then we get empty array, this is a nice way of setting default value, good for when we doing have hardcoded data available so we set defaults
+
+for startermenu we mutated the name of the object as shown below*/
+
+// const { menu: mains = [], starterMenu: starters = [] } = restaurant;
+
+// console.log(starters, mains);
+
+
+/* _____________________________________ */
+
 
 // console.log(resturantName,restaurantFoodCategories,restaurantMainMenu, restaurantOpeningHrs);
-
-
 
 //nested objects
 
@@ -68,13 +108,13 @@ const {name: resturantName, categories: restaurantFoodCategories,openingHours: r
 // let a = 234;
 // let b = 123;
 // const obj = { a: 23, b: 12, c: 14 };
+/* _____________________________________ */
 
 //this is the way we destructure and reassign variables to objects that are one liners
 // ({a,b } = obj)
 // console.log(a,b);
 
-
-/*///////////////////////////////////// */
+/* _____________________________________ */
 
 // const arr = [2,3,4];
 // const [x,y,z] = arr;
@@ -82,3 +122,5 @@ const {name: resturantName, categories: restaurantFoodCategories,openingHours: r
 
 // const [first,, second] = restaurant.categories;
 // console.log(first,second);
+
+/* _____________________________________ */
